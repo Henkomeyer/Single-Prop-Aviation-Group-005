1fd850a29f5b0014828e9c56cc2c78f50d040930
George <georgepieterse15@gmail.com>
2023-10-18 21:50:27 +0200

    Quiz Questions
    Added some more Quiz Questions

--- react-website/src/extra/Questions.js
+++ react-website/src/extra/Questions.js
@@ -23,11 +23,45 @@ export const Questions = [
         optionD :"Secondary Checklist",
         answer:"A"
     },
-   { prompt:"Which of the following checklist formats are the most used in aviation?",
-    optionA:"Vocal Checklist",
-    optionB: "Paper Checklist" ,
-    optionC :"Scroll Checklist",
-    optionD :"Mechanical Checklist",
-    answer:"B"
-},
+    { 
+        prompt:"Which of the following checklist formats are the most used in aviation?",
+        optionA:"Vocal Checklist",
+        optionB: "Paper Checklist" ,
+        optionC :"Scroll Checklist",
+        optionD :"Mechanical Checklist",
+        answer:"B"
+    },
+    {
+        prompt: "What does ATC stand for in aviation?",
+        optionA: "Air Traffic Control",
+        optionB: "Aviation Technical Committee",
+        optionC: "Aircraft Takeoff Control",
+        optionD: "Airborne Traffic Coordination",
+        answer: "A"
+    },
+    {
+        prompt: "What is the purpose of the VFR (Visual Flight Rules) in aviation?",
+        optionA: "To regulate air traffic at night",
+        optionB: "To control flights over mountainous areas",
+        optionC: "To establish communication protocols",
+        optionD: "To govern aircraft operations in visual conditions",
+        answer: "D"
+    },
+    {
+        prompt: "In aviation, what does ILS stand for?",
+        optionA: "Instrument Landing System",
+        optionB: "International Landing Standards",
+        optionC: "Integrated Logistics Support",
+        optionD: "Inflight Landing Sequence",
+        answer: "A"
+    },
+    {
+        prompt: "What is the primary function of the altimeter in an aircraft?",
+        optionA: "Measure airspeed",
+        optionB: "Indicate altitude",
+        optionC: "Control engine power",
+        optionD: "Monitor fuel consumption",
+        answer: "B"
+    }
+    
 ]
