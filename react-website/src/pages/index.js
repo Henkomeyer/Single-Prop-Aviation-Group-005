579c2116c7de5faaefa116763e9b36fd7e5434ec
Henko Meyer <84342604+Henkomeyer@users.noreply.github.com>
2023-10-19 16:49:41 +0200

    Added Another Section

--- react-website/src/pages/index.js
+++ react-website/src/pages/index.js
@@ -1,5 +1,8 @@
 import React from 'react';
-import BackImage from './images/single-prop-plane.jpeg'
+import BackImage from './images/backGround.svg';
+import photo1 from './images/console.jpg';
+import photo2 from './images/redplane.jpg';
+import "./css/home.css";
 
 const Home = () => {
   return (
@@ -20,7 +23,7 @@ const Home = () => {
           <div style={{ width: "100%", height: 370, left: "3%", top: 40, position: 'absolute' }}>
             <div style={{ width: "100%", height: 135, left: 143, top: 0, position: 'absolute' }}>
               <div style={{ width: 120, height: 135, left: 0, top: 0, position: 'absolute', background: '#fff', textAlign: "center", paddingTop: '4%' }}>
-              <svg xmlns="http://www.w3.org/2000/svg" width="80" height="80" viewBox="0 0 24 24" fill="none" stroke="#3D7EA4" stroke-width="1.5" stroke-linecap="round" stroke-linejoin="round"><path d="M14 2H6a2 2 0 0 0-2 2v16c0 1.1.9 2 2 2h12a2 2 0 0 0 2-2V8l-6-6z"/><path d="M14 3v5h5M16 13H8M16 17H8M10 9H8"/></svg>              </div>
+                <svg xmlns="http://www.w3.org/2000/svg" width="80" height="80" viewBox="0 0 24 24" fill="none" stroke="#3D7EA4" stroke-width="1.5" stroke-linecap="round" stroke-linejoin="round"><path d="M14 2H6a2 2 0 0 0-2 2v16c0 1.1.9 2 2 2h12a2 2 0 0 0 2-2V8l-6-6z" /><path d="M14 3v5h5M16 13H8M16 17H8M10 9H8" /></svg>              </div>
             </div>
             <div style={{ width: 395, height: 158, left: 10, top: 185, position: 'absolute', textAlign: 'center' }}>
               <span style={{ color: 'black', fontSize: 36, fontFamily: 'Montserrat', fontWeight: '700', wordWrap: 'break-word' }}>Interactive Learning<br /></span>
@@ -29,7 +32,7 @@ const Home = () => {
           </div>
           <div style={{ height: 370, paddingBottom: 27, paddingLeft: 10, paddingRight: 10, left: "39%", top: 42, position: 'absolute', flexDirection: 'column', justifyContent: 'flex-start', alignItems: 'center', gap: 50.52, display: 'inline-flex' }}>
             <div style={{ width: 137.60, height: 134.48, background: '#fff', textAlign: 'center', paddingTop: '12%' }}>
-            <svg xmlns="http://www.w3.org/2000/svg" width="80" height="80" viewBox="0 0 24 24" fill="none" stroke="#3D7EA4" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"><circle cx="12" cy="12" r="10"></circle><path d="M9.09 9a3 3 0 0 1 5.83 1c0 2-3 3-3 3"></path><line x1="12" y1="17" x2="12.01" y2="17"></line></svg>
+              <svg xmlns="http://www.w3.org/2000/svg" width="80" height="80" viewBox="0 0 24 24" fill="none" stroke="#3D7EA4" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"><circle cx="12" cy="12" r="10"></circle><path d="M9.09 9a3 3 0 0 1 5.83 1c0 2-3 3-3 3"></path><line x1="12" y1="17" x2="12.01" y2="17"></line></svg>
             </div>
             <div style={{ width: 395, height: 158, textAlign: 'center' }}>
               <span style={{ color: 'black', fontSize: 36, fontFamily: 'Montserrat', fontWeight: '700', wordWrap: 'break-word' }}>Guidelines<br /></span>
@@ -41,8 +44,8 @@ const Home = () => {
               <span style={{ color: 'black', fontSize: 36, fontFamily: 'Montserrat', fontWeight: '700', wordWrap: 'break-word' }}>Support<br /></span>
               <span style={{ color: 'black', fontSize: 32, fontFamily: 'Montserrat', fontWeight: '400', wordWrap: 'break-word' }}>Support in the form of FAQ and contact form.</span>
             </div>
-            <div style={{ width: 126.32, height: 118, left: '34%', top: 14, position: 'absolute', background: '#fff',textAlign: 'center',paddingTop: '10%' }}>
-            <svg xmlns="http://www.w3.org/2000/svg" width="80" height="80" viewBox="0 0 24 24" fill="none" stroke="#3D7EA4" stroke-width="2.5" stroke-linecap="round" stroke-linejoin="round"><circle cx="11" cy="11" r="8"></circle><line x1="21" y1="21" x2="16.65" y2="16.65"></line></svg>            </div>
+            <div style={{ width: 126.32, height: 118, left: '34%', top: 14, position: 'absolute', background: '#fff', textAlign: 'center', paddingTop: '10%' }}>
+              <svg xmlns="http://www.w3.org/2000/svg" width="80" height="80" viewBox="0 0 24 24" fill="none" stroke="#3D7EA4" stroke-width="2.5" stroke-linecap="round" stroke-linejoin="round"><circle cx="11" cy="11" r="8"></circle><line x1="21" y1="21" x2="16.65" y2="16.65"></line></svg>            </div>
           </div>
         </div>
         <div style={{ width: '100%', height: 118, paddingTop: 17, paddingLeft: 6, justifyContent: 'center', paddingRight: 15, top: 114, position: 'absolute', background: 'white', justifyContent: 'flex-start', alignItems: 'center', display: 'inline-flex' }}>
@@ -51,6 +54,45 @@ const Home = () => {
             <span style={{ color: 'black', fontSize: 85, fontFamily: 'Mukta', fontWeight: '700', wordWrap: 'break-word' }}>Offer</span>
           </div>
         </div>
+      </div>
+      {/* Start of new section */}
+      <div style={{ width: '100%', height: 1156, background: '#fff', position: 'relative' }}>
+        <div style={{ width: '38%', height: 983, left: 112, top: 89, position: 'absolute', background: 'white', boxShadow: '-5px 8px 20px rgba(0, 0, 0, 0.10)', borderRadius: 40, overflow: 'hidden' }}>
+          <div style={{ width: 493, height: 88, left: 121, top: '5%', position: 'absolute', textAlign: 'center', color: '#3E80A5', fontSize: '280%', fontFamily: 'Istok Web', fontWeight: '700', wordWrap: 'break-word' }}>Assisting Pilots</div>
+          <div style={{ width: 485, height: 61, left: 107, top: '76%', position: 'absolute', textAlign: 'center' }}>
+            <span style={{ color: 'black', fontSize: 48, fontFamily: 'Istok Web', fontWeight: '700', wordWrap: 'break-word' }}>Our</span>
+            <span style={{ color: '#3E80A5', fontSize: 48, fontFamily: 'Istok Web', fontWeight: '700', wordWrap: 'break-word' }}> Stakeholders</span>
+          </div>
+          <div style={{ width: 379, height: 65, left: '17%', top: '10%', position: 'absolute', textAlign: 'center', color: '#1E1E1E', fontSize: 48, fontFamily: 'Istok Web', fontWeight: '400', wordWrap: 'break-word' }}>around the globe</div>
+          <div style={{ width: 665, height: 560, left: 40, top: 186, position: 'absolute' }}>
+            <div style={{ width: 647, height: 199, left: 18, top: 0, position: 'absolute' }}>
+              <span style={{ color: '#3E80A5', fontSize: '150%', fontFamily: 'Istok Web', fontWeight: '700', wordWrap: 'break-word' }}>Our Mission</span>
+              <span style={{ color: 'black', fontSize: '120%', fontFamily: 'Istok Web', margin: '5%', fontWeight: '600', wordWrap: 'break-word' }}><br></br>Our mission at Aviation is to empower single prop aviation enthusiasts with the knowledge, tools, and resources to prioritize safety above all else, ensuring each flight is a secure and exhilarating experience.</span>
+            </div>
+            <div style={{ width: 647, height: 199, left: 18, top: '33%', position: 'absolute' }}>
+              <span style={{ color: '#3E80A5', fontSize: '150%', fontFamily: 'Istok Web', fontWeight: '700', wordWrap: 'break-word' }}>Vision</span>
+              <span style={{ color: 'black', fontSize: '120%', fontFamily: 'Istok Web', margin: '5%', fontWeight: '600', wordWrap: 'break-word' }}><br /> Our vision is a single prop aviation community where safety is ingrained in every aspect of flight, creating a world where pilots, beginners, and experts alike can enjoy the freedom of the skies with confidence and peace of mind.</span>
+            </div>
+            <div style={{ width: 647, height: 100, left: 18, top: '68%', position: 'absolute' }}>
+              <span style={{ color: '#3E80A5', fontSize: '150%', fontFamily: 'Istok Web', fontWeight: '700', wordWrap: 'break-word' }}>Values</span>
+              <span style={{ color: 'black', fontSize: '120%', fontFamily: 'Istok Web', margin: '5%', fontWeight: '600', wordWrap: 'break-word' }}><br />Safety, Education, Community.</span>
+            </div>
+          </div> 
+          
+          <div style={{ paddingTop: 14, paddingBottom: 25, paddingLeft: 17, paddingRight: 2, left: 46, top: 770, position: 'absolute', justifyContent: 'flex-end', alignItems: 'flex-end', display: 'inline-flex' }}>
+            <div style={{ width: 304, paddingRight: 8, justifyContent: 'flex-start', alignItems: 'center', display: 'inline-flex' }}>
+              <a href="https://www.belgiumcampus.ac.za" target="_blank" rel="noopener noreferrer">
+                <img style={{ width: 296, height: 168 }} src="https://www.belgiumcampus.ac.za/wp-content/uploads/2022/04/Artboard-5.png" alt="Your Alt Text" />
+              </a>            </div>
+            <div style={{ width: 320, top:'5%', height: 124, paddingLeft: '16%', color: 'black', fontSize: '120%', fontFamily: '', fontWeight: '400', wordWrap: 'break-word' }}>
+              - Belgium Campus<br />
+              - FlyedSafair<br />
+              - Pesang
+            </div>
+          </div>
+        </div>
+        <img style={{ width: 520, height: 896, left: 920, top: '15%', position: 'absolute', background: 'linear-gradient(0deg, white 0%, white 100%)', borderRadius: 1000 }} src={photo1} />
+        <img style={{ width: 497, height: 806, left: 1332, top: '10%', position: 'absolute', background: 'linear-gradient(0deg, white 0%, white 100%)', borderRadius: 1000 }} src={photo2} />
       </div>
 
     </div>
