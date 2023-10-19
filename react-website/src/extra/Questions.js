03f4691be518f421feb56c803c1a21ca85f6c129
HenkoSteyn <91952738+HenkoSteyn@users.noreply.github.com>
2023-10-19 10:21:37 +0200

    Added questions for quiz

--- react-website/src/extra/Questions.js
+++ react-website/src/extra/Questions.js
@@ -7,6 +7,16 @@ export const Questions = [
         optionD :"8",
         answer:"D"
     },
+
+    {
+        prompt:"What percentage of aircraft accidents are believed to be related to human error or human performance issues?",
+        optionA:"50%-60%",
+        optionB: "75%-80%" ,
+        optionC :"20%-25%",
+        optionD :"100%",
+        answer:"B"
+    },
+
     {
         prompt:"Which of the following is not a section on the paper checklist?",
         optionA:"Taxi",
@@ -40,6 +50,14 @@ export const Questions = [
         answer: "A"
     },
     {
+        prompt: "In regards of situational awareness errors, which level of error occurs most frequently in aviation accidents?",
+        optionA: "Level 1",
+        optionB: "Level 2",
+        optionC: "Level 3",
+        optionD: "All levels occur equally",
+        answer: "A"
+    },
+    {
         prompt: "What is the purpose of the VFR (Visual Flight Rules) in aviation?",
         optionA: "To regulate air traffic at night",
         optionB: "To control flights over mountainous areas",
@@ -48,12 +66,28 @@ export const Questions = [
         answer: "D"
     },
     {
+        prompt: "What is the primary cause of errors related to situational awareness?",
+        optionA: "Pilot inattention",
+        optionB: "Lack of communication",
+        optionC: "Relevant data not being available to the pilot",
+        optionD: "Technical failures",
+        answer: "C"
+    },
+    {
         prompt: "In aviation, what does ILS stand for?",
         optionA: "Instrument Landing System",
         optionB: "International Landing Standards",
         optionC: "Integrated Logistics Support",
         optionD: "Inflight Landing Sequence",
         answer: "A"
+    },
+    {
+        prompt: "In a study on pilot training, what was identified as the main influence contributing to pilot fatigue?",
+        optionA: "Age",
+        optionB: "Lack of proper training",
+        optionC: "Long working days and insufficient sleep",
+        optionD: "Gender",
+        answer: "C"
     },
     {
         prompt: "What is the primary function of the altimeter in an aircraft?",
