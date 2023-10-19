43f19d63c4acbab1193247324945033780bcf129
George <georgepieterse15@gmail.com>
2023-10-19 16:23:41 +0200

    Added Best Practices to the Blog

--- react-website/src/pages/blogs.js
+++ react-website/src/pages/blogs.js
@@ -57,7 +57,49 @@ const Blogs = () => {
             Situation Awareness (SA) is a crucial factor in effective decision-making, especially in the dynamic flight environment. A study was conducted to determine the most common errors related to situational awareness. The study made use of the Aviation Safety Reporting System (ASRS) database and its data related to situational awareness. 
             Three categories were created for the study being Level 1 (failure to correctly perceive the information), Level 2 (failure to comprehend the situation), or Level 3 (failure to project the situation into the future). The study indicated that 76.3% of the time the error fell in level 1, 20.3% in level 2 and only 3.4% of the time in level 3.
              So, the results indicate that most of the time errors related to situational awareness is a result of relevant data not being available to the pilot.
-          </p> 
+          </p>
+	  </div>
+    </div>
+    <div class="card">
+    <h2>Best Practices</h2>
+      <h5>Best practices to ensure safe operations in single-prop aviation.</h5>
+      <div class="img">
+      <image></image>
+      </div>
+	  <div>
+      <p>
+            <h5>Comprehensive Pre-flight Inspections</h5>
+          For a single-prop aircraft, a comprehensive pre-flight inspection is a fundamental component of aviation safety. Pilots carefully go over control surfaces, engine parts, and fuel systems using a checklist. 
+          This procedure is essential for spotting and fixing possible problems prior to takeoff, guaranteeing that the aircraft is operating at peak efficiency. 
+          Pre-flight inspection diligence is a proactive measure that lowers the likelihood of in-flight malfunctions or failures significantly, improving pilot and passenger safety overall. </p> <br></br>
+          <p>
+            <h5>Continuous Training and Proficiency</h5>
+            Responsible controlling of a single-prop airplane requires a dedication to ongoing training and proficiency development. 
+            Pilots need to keep up with the latest developments in aviation technology and safety protocols. 
+            Pilots are kept prepared for a range of situations through frequent training sessions and proficiency exams, which cover everything from emergency procedures to using sophisticated navigational aids. 
+            A culture of safety in aviation is fostered by this proactive approach, which not only improves the pilot's abilities but also gives passengers a sense of confidence.
+          </p> <br></br>
+          <p>
+            <h5>Terrain Awareness and Avoidance</h5>
+            Emphasizing terrain and obstacle awareness is crucial for the safe operation of single-prop airplanes. 
+            Pilots must complete extensive training in order to comprehend the significance of terrain considerations when planning flights. 
+            Pilots can reduce the likelihood of collisions with obstacles by improving their situational awareness through the use of advanced terrain awareness technology. 
+            This best practice serves as an example of a proactive approach to safety, in which pilots receive real-time terrain information in addition to training and equipment.
+          </p> <br></br>
+          <p>
+            <h5>Communication Protocols</h5>
+            In aviation, good communication is essential, and pilots of single-prop aircraft need to comply with the correct radio phraseology and communication protocols. 
+            This urged procedure guarantees precise and clear communication with air traffic control, particularly in places with limited coverage. 
+            Pilots contribute to the overall safety of airspace by avoiding misunderstandings and enabling seamless coordination by adhering to established communication protocols. 
+            An essential component of a safe and well-run flight is effective and clear communication.
+          </p>
+          <p>
+            <h5>Flight Planning and Navigation</h5>
+            Thorough pre-flight planning, including route analysis, fuel considerations, and weather assessments, is a foundational best practice. 
+            Pilots of single-prop aircraft use sophisticated navigational aids to improve route efficiency and safety during the flight. 
+            The risk posed by unexpected weather or route deviations is reduced by this careful planning. 
+            An example of a commitment to making the most of the resources at hand for safer and more effective single-prop airplane operations is the integration of technology into navigation.
+          </p>
 	  </div>
     </div>
   </div>
